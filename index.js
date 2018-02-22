56aa5c625641d28929b42bb9a61290edd7680e93
AfanasyPopov <afanaasy.popov@gmail.com>
2018-02-22 11:55:28 +0300

    getProjectsList and addProject is developed correctly v.1

--- index.js
+++ index.js
@@ -154,6 +154,101 @@ app.get('/', function(req, res, next) {
   // Handle the get for this route
 });
 
+//----start-----------PROJECT'S EVENTS---------------------------
+/** req=>: { account_key: '5a4b298a-f903-4fbe-b53b-049b1a0d8d9b' } */
+app.post('/getProjectsList', function(req, res){
+    console.log('getProjectsList req.body:', req.body);
+    const makeAsyncRequest = async () => {
+        account_key = req.body.account_key;
+        console.log('getProjectsList user:', account_key);
+        havePermission = await isHavePermission(account_key);
+        accountUserId= await getUserId(account_key);
+        usersList=await getUsersList();
+        console.log('isHavePermission:'+ havePermission+' , id:'+accountUserId)
+        if(havePermission) {
+           projectsList = await db.any("SELECT p.id,p.project_name,p.manager,p.parent,p.description,concat( u.last_name,' ',u.username) as owner_user, false as show FROM projects p INNER JOIN users u ON (p.owner_user=u.id)",[ ])
+           console.log('getProjectsList projectsList[0]:', projectsList[0]);
+           usersList= await getUsersList();
+           projectsList.forEach(element => { //conver manager [array] to  String Names of UsersList 
+            element.manager.forEach(function(elem,ind,arr){
+                usersList.findIndex(function (index){
+                    if (index.id==elem){
+                        element.manager[ind]=' '+index.last_name+' '+index.username;
+                    }
+                })
+            })
+           });
+        }
+        if (projectsList.length>0){
+            data={
+                projectsList: projectsList,
+                mess:"Список проектов получен!"
+            }
+        } else {
+           if(!havePermission) {data.mess="Отказано в доступе"}
+        }
+        //console.log('getProjectsList data:', data);
+        res.send(data); 
+    }
+    makeAsyncRequest()
+})
+    /**formData -> req=>(
+        account_key:"5a4b298a-f903-4fbe-b53b-049b1a0d8d9b"
+        contragentCostumer:""
+        contragentOur:""
+        profilePic:""
+        projectDescription:""
+        projectManager:""
+        projectName:"явсяфвы")*/
+app.post('/addProject', function(req, res){
+            const makeAsyncRequest = async () => {
+                account = req.body;
+                console.log('addProject account:', account);
+                havePermission = await isHavePermission(account.account_key);
+                ownerUserId= await getUserId(account.account_key);
+                console.log('isHavePermission:'+ havePermission+' , id:'+ownerUserId)
+                if(havePermission) {
+                   addedProject = await db.any('INSERT INTO projects (  project_name, owner_user, manager, description) VALUES (   $1, $2, $3, $4 ) RETURNING *',[account.projectName,ownerUserId,account.projectManager,account.projectDescription ])
+                 }
+                if (addedProject.length>0){
+                    mess=[{mess:"Проект успешно создан!"}]
+                } else {
+                    mess=[{mess:"ОШИБКА СОЗДАНИЯ ПРОЕКТА!"}]
+                }
+                res.send(mess); 
+            }
+            makeAsyncRequest()
+})
+
+/*var data={
+    account_key : this.myApp.user.uuid_key,
+    projectId:item
+  }*/
+app.post('/delProject', function(req, res){
+    const makeAsyncRequest = async () => {
+        account = req.body;
+        console.log('addProject account:', account);
+        havePermission = await isHavePermission(account.account_key);
+        ownerUserId= await getUserId(account.account_key);
+        console.log('delProject havePermission:'+ havePermission+' , ownerUserId:'+ownerUserId)
+        if(havePermission) {
+           deleledProject = await db.any('DELETE FROM projects where id=$1 RETURNING *',[ account.projectId])
+         }
+        if (deleledProject.length>0){
+            mess=[{mess:"Проект успешно УДАЛЕН!"}]
+        } else {
+            mess=[{mess:"Ошибка удаления проекта!"}]
+        }
+        res.send(mess); 
+    }
+    makeAsyncRequest()
+})
+        
+
+
+//----end-----------PROJECT'S EVENTS---------------------------
+
+//----start-----------LOGIN---------------------------
 app.post('/login', function(req, res){
 	console.log('Express:'+JSON.stringify(req.body));      // your JSON
     console.log('login event: ' + req.body.email +';'+req.body.password);
@@ -177,32 +272,60 @@ app.post('/login', function(req, res){
 
 	console.log('login event/SQL request not started');
 });
+app.post('/getUserList', function(req, res){
+    console.log("getUserList begin");
+    const makeRequest = async () => {
+        var dataWithDir ={
+            data: [],
+            users:[],
+            dir:{
+                organization:'',
+                role_in_project:'',
+                status:''
+            }
+        };    
+        console.log('getUserList async start:------"'+JSON.stringify(dataWithDir));
+        dataWithDir.dir.organization = await  db.any('SELECT id as value,org_short_name as label  FROM contragents c ORDER BY c.id ASC');
+        dataWithDir.dir.role_in_project = await  db.any('SELECT ur.id as value, ur.caption as label FROM user_roles ur	ORDER BY ur.id ASC');
+        dataWithDir.dir.status = await db.any('SELECT us.id as value, us.caption as label FROM user_status us ORDER BY us.id ASC');
+        dataWithDir.users = await db.any("SELECT u.id, u.uuid_key, u.username, u.last_name, u.email, u.active, u.description, u.contragent_flag, u.user_flag, u.group_flag, contr.org_short_name organization,u.img_id, ur.caption as role_in_project, us.caption as status,concat( f.id,'.',f.extention )as url FROM users u INNER JOIN user_roles ur ON ( u.role_in_project = ur.id  )  INNER JOIN user_status us ON ( u.status = us.id  ) LEFT OUTER JOIN files f ON( u.img_id =f.id) LEFT OUTER JOIN contragents contr ON( u.organization =contr.id)",[]);
+        console.log('async end:------"'+JSON.stringify(dataWithDir));
+        res.send(JSON.stringify(dataWithDir));    // send SQL result    
+      }
+      console.log(" getUserList before makeRequest() ");
+      makeRequest()
+        .catch(err => {
+            console.log(err);
+        })
+    console.log("getUserList end");
+});
 
+//----end-----------LOGIN---------------------------
+
+//----start-----------USER'S EVENTS---------------------------
 app.post('/addUser', function(req, res){
     var item =req.body.item;
-    sqlReq="WITH x AS (INSERT INTO user_cred (user_key) VALUES (uuid_generate_v4()) RETURNING user_key),"+
-      "y AS (INSERT INTO users ( uuid_key,username,last_name,email,status,role_in_project)"+
-      "SELECT user_key,$1,$2,$3,0,0 FROM x RETURNING *)"+
-      "SELECT * FROM y;";
-      db.any(sqlReq,[item.name, item.last_name, item.email])
-      .then(data=>{
-        console.log('Express addUser res:'+JSON.stringify(data));  
-        res.send(JSON.stringify(data));    // echo the result back
+    const makeAsyncRequest = async () => {
+        sqlReq="WITH x AS (INSERT INTO user_cred (user_key) VALUES (uuid_generate_v4()) RETURNING user_key),"+
+        "y AS (INSERT INTO users ( uuid_key,username,last_name,email,status,role_in_project)"+
+        "SELECT user_key,$1,$2,$3,0,0 FROM x RETURNING *)"+
+        "SELECT * FROM y;";
+        addedUserData = await db.any(sqlReq,[item.name, item.last_name, item.email])
+        console.log('Express addUser res:'+JSON.stringify(addedUserData));  
         
         if (req.body.item.profilePic){
             var bodyData = req.body.item.profilePic;
             var imageBuffer = decodeBase64Image(bodyData);
-            updateUserImg(data[0].uuid_key,imageBuffer);
+            img_id=await updateUserImg(addedUserData[0].uuid_key,imageBuffer);
             console.log('addUser updateUserImg:', imageBuffer.type);
-            console.log('addUser req.body.item.uuid_key:', JSON.stringify(data[0].uuid_key));
+            console.log('addUser addedUserData.uuid_key:', JSON.stringify(addedUserData[0].uuid_key));
+            console.log('updateUserafter updateUserImg -> img_id:', img_id[0]);
+            await db.any('UPDATE users SET img_id=$1 WHERE uuid_key=$2',[img_id[0],addedUserData[0].uuid_key])
         }
-    
-        })
+        res.send(JSON.stringify(addedUserData));    // echo the result back
+    }
+    makeAsyncRequest()
 });
-app.post('/updateUserImg', function (req, res) {
-    var item =req.body.item;
-    updateUserImg(item.uuid_key,item.url);
-})
 app.post('/delUser', function(req, res){
     console.log('Express delUser:'+JSON.stringify(req.body));      // your JSON
     var item =req.body.item;
@@ -282,31 +405,7 @@ app.post('/passRepSetPass', function(req, res){
 })
 
 
-app.post('/getUserList', function(req, res){
-    console.log("getUserList begin");
-    const makeRequest = async () => {
-        var dataWithDir ={
-            data: [],
-            users:[],
-            dir:{
-                role_in_project:'',
-                status:''
-            }
-        };    
-        console.log('getUserList async start:------"'+JSON.stringify(dataWithDir));
-        dataWithDir.dir.role_in_project = await  db.any('SELECT ur.id as value, ur.caption as label FROM user_roles ur	ORDER BY ur.id ASC');
-        dataWithDir.dir.status = await db.any('SELECT us.id as value, us.caption as label FROM user_status us ORDER BY us.id ASC');
-        dataWithDir.users = await db.any("SELECT u.id, u.uuid_key, u.username, u.last_name, u.email, u.active, u.description, u.contragent_flag, u.user_flag, u.group_flag, u.organization,u.img_id, ur.caption as role_in_project, us.caption as status,concat( f.id,'.',f.extention )as url FROM users u INNER JOIN user_roles ur ON ( u.role_in_project = ur.id  )  INNER JOIN user_status us ON ( u.status = us.id  ) LEFT OUTER JOIN files f ON( u.img_id =f.id)",[]);
-        console.log('async end:------"'+JSON.stringify(dataWithDir));
-        res.send(JSON.stringify(dataWithDir));    // send SQL result    
-      }
-      console.log(" getUserList before makeRequest() ");
-      makeRequest()
-        .catch(err => {
-            console.log(err);
-        })
-    console.log("getUserList end");
-});
+//----end-----------USER'S EVENTS---------------------------
 
 function objToSQLforUpdate (item){
     count=0;
@@ -342,6 +441,8 @@ function decodeBase64Image(dataString) {
 }
 
 function updateUserImg (uuidKey, imgBuf){
+    console.log("updateUserImg uuidKey:", uuidKey);
+    console.log("updateUserImg imgBuf:", imgBuf);
     const makeAsyncRequest = async () => {
         if (imgBuf.type==="image/jpeg") {var extention = 'jpg'} else {if (imgBuf.type==='image/png') {var extention = 'png'} else { extention=''}};
         var fileId = await db.any('INSERT INTO files (file_type, extention) values ($1,$2) RETURNING id',[imgBuf.type,extention]);
@@ -369,6 +470,46 @@ function updateUserImg (uuidKey, imgBuf){
             console.log(err);
         })
   }
+function isHavePermission(key){
+    console.log('isHavePermission key: ', key)
+    const makeAsyncRequest = async () => {
+        data= await db.any('select uuid_key, role_in_project from users where uuid_key=$1',[key])
+        console.log('isHavePermission data: ', data)
+        if (data[0].role_in_project===6) {
+            return true
+            console.log('isHavePermission:'+ true+' , key:'+data[0].uuid_key+', role_in_project:'+data[0].role_in_project)
+         } else {
+            return false
+            console.log('isHavePermission:'+ false+' , key:'+data[0].uuid_key+', role_in_project:'+data[0].role_in_project)
+         }
+     }
+   return makeAsyncRequest()
+}
+function getUserId(key){
+    console.log('isHavePermission key: ', key)
+    const makeAsyncRequest = async () => {
+        data= await db.any('select id from users where uuid_key=$1',[key])
+        console.log('isHavePermission data: ', data)
+        if (data[0].id) {
+            return data[0].id
+            console.log('getUserId:'+data[0].id)
+         } else {
+            return false
+            console.log('getUserId:'+false)
+         }
+     }
+   return makeAsyncRequest()
+}
+function getUsersList(){
+    console.log('getUsersList start')
+    const makeAsyncRequest = async () => {
+        data= await db.any('select id, last_name, username from users ',[])
+        console.log('getUsersList data: ', data)
+        return data;
+    }
+   return makeAsyncRequest()
+}
+
 app.listen(port);
 console.log("EXPRESS: Listening on port:" + port);
 
